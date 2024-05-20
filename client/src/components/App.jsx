a780820f7b14ffcfa3b0b7e7423394fc672ede8d
SarahC233 <122423221+SarahC233@users.noreply.github.com>
2024-05-20 17:10:22 +1200

    updates to files in client directory with images and some additional files

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import FileUpload from "./FileUpload";
+import styles from "./App.module.css";
+import logoImage from "../images/turners_logo.png";
+import heroImage from "../images/turners_hero.png"; 
 
 const App = () => {
   return (
     <div>
-      <h1>Car Type Predictor</h1>
-      <FileUpload />
+      <header className={styles.header}>
+        <h1>
+          <img src={logoImage} alt="Logo" /> Car Type Predictor
+        </h1>
+      </header>
+      <img
+        className={styles.heroImage}
+        src={heroImage} 
+        alt="Hero"
+      />
+      <div className={styles.fileUploadArea}>
+        <FileUpload />
+      </div>
     </div>
   );
 };
